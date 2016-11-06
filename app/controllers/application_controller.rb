d30ac7cd9d3519b5f6dc7d1c2112e96d326738b0
Alicia MacLennan <aflyingfemme@gmail.com>
2016-11-06 11:51:08 -0800

    Add practice route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡hola, world!"
+  end
+
+  def goodbye
+    render html: "¡adios, mundo!"
+  end
 end
